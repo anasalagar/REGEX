370d1e6e00b5c92fbd4f42ce31f46248d54e5911
anasalagar <90851784+anasalagar@users.noreply.github.com>
2021-11-05 19:04:16 +0530

    program.cs file added

---
+++ program.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Regex_First_Name_UC_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your first name");
+
+            string firstname = Console.ReadLine();
+            UC_1_First_Name .FirstName(firstname);
+         
+
+        }
+    }
+}
