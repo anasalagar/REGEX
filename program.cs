d5f36b7d61ea4c1f86cd1c0d636842a3c02837d6
anasalagar <90851784+anasalagar@users.noreply.github.com>
2021-11-06 02:55:06 +0530

    program.cs file added

---
+++ program.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Regex_Last_Name_UC2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your First name : ");
+            string f = Console.ReadLine();
+            check.firstName(f);
+
+
+            Console.WriteLine("Enter your Last name : ");
+            string l = Console.ReadLine();
+            check.LastName(l);
+
+            Console.WriteLine("Enter your Email address : ");
+            string e = Console.ReadLine();
+            check.Email(e);
+
+            Console.WriteLine("Enter your mobile number : ");
+            string p = Console.ReadLine();
+            check.Mobile(p);
+        }
+    }
+}
