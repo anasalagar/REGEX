4ab23f10491b3ce2adfbd09dfd051396244afa65
anasalagar <90851784+anasalagar@users.noreply.github.com>
2021-11-07 23:58:11 +0530

    program.cs file added

---
+++ program.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Regex_Last_Name_UC2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                 Console.WriteLine("Enter a password");
+            string w = Console.ReadLine();
+            check.password(w);
+        }
+    }
+}
